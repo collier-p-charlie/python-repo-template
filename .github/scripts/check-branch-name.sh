8d48ed7f035b178027e9e38339f8a96f05d8138e
charliecollier <collier.p.charlie@gmail.com>
2025-08-08 16:29:39 +0100

    update pre-commit config for branch naming locally enhanced checking branch name script update rest of scripts

--- .github/scripts/check-branch-name.sh
+++ .github/scripts/check-branch-name.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-BRANCH_NAME="$1"
-echo "Checking branch name: $BRANCH_NAME"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+SOURCE_BRANCH=${1:-$CURRENT_BRANCH}
+TARGET_BRANCH=$2
 
-if [[ ! $BRANCH_NAME =~ ^(feature|bugfix)/.+$ ]]; then
-  echo "Branch name must start with 'feature/' or 'bugfix/'" && exit 1
+if [[ ! $SOURCE_BRANCH =~ ^(feature|bugfix|hotfix|release)/.+$ ]] && [[ ! $SOURCE_BRANCH == develop ]]; then
+  echo "Branch name $SOURCE_BRANCH must be of the form '^(feature|bugfix|hotfix|release)/*' or 'develop'"
+  exit 1
 fi
 
-echo "Branch name is valid"
+if [[ $TARGET_BRANCH ]]; then
+  echo "Checking source branch $SOURCE_BRANCH against target branch $TARGET_BRANCH"
+
+  if [[ $TARGET_BRANCH =~ develop ]]; then
+    if [[ ! $SOURCE_BRANCH =~ ^(feature|bugfix)/.+$ ]]; then
+      echo "Source branch must be of the form '^(feature|bugfix)/*' when targeting 'develop'" && exit 1
+    fi
+  elif [[ $TARGET_BRANCH =~ main ]]; then
+    if [[ ! $SOURCE_BRANCH =~ ^(hotfix|release)/.+$ ]] && [[ ! $SOURCE_BRANCH == develop ]]; then
+      echo "Source branch must be of the form '^(hotfix|release)/*' or be 'develop' when targeting 'main'" && exit 1
+    fi
+  else
+    echo "Target branch must be either 'develop' or 'main'" && exit 1
+  fi
+fi
+
+echo "Branch name $SOURCE_BRANCH is valid"
